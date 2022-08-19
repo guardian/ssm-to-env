9f9d42aa317fb467c7ee8693f065639998b2f929
Robert Kenny <robert.kenny@guardian.co.uk>
2022-08-19 16:42:10 +0100

    script needs to locate binaries in context

--- wrapper-script/ssm-to-env.sh
+++ wrapper-script/ssm-to-env.sh
@@ -3,15 +3,20 @@
 # The path to the interpreter and all of the originally intended arguments
 args=("$@")
 
+# Read prefix from env handed to lambda
 prefix="${SSM_PATH_PREFIX}"
 
+# The name of this script
 name=$(basename $0)
+
+# The full path to this script
+fullPath="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Get architecture so we can choose the correct binary
 arch=$(uname | tr '[:upper:]' '[:lower:]')
 
 # Write env vars to temporary file
-./nest-secrets-${arch}-amd64 --prefix ${prefix} > /tmp/${name}.env
+${fullPath}/nest-secrets-${arch}-amd64 --prefix ${prefix} > /tmp/${name}.env
 
 # Export all the values to env vars
 set -o allexport
